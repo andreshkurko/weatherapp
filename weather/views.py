6d76c7a4b819ca740c2f365d696e42927dcf02af
CodingDevHub <codingdevhub@gmail.com>
2019-08-31 23:45:02 +0300

    add alert message info

--- weather/views.py
+++ weather/views.py
@@ -8,9 +8,35 @@ def index(request):
     appid = '87f0131b424f6fd3a4c9f08a7d961bde'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
+    err_msg = ''
+    message = ''
+    message_class = ''
+
     if (request.method == 'POST'):
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                print(r)
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City already exist in the word!'
+            else:
+                err_msg = 'City already exist in the database!'
+
+        if err_msg:
+            message = err_msg
+            message_class = 'alert-danger'
+        else:
+            message = 'City added successfully!'
+            message_class = 'alert-success'
+
+    print(err_msg)
 
     form = CityForm()
 
@@ -28,8 +54,10 @@ def index(request):
 
         all_cities.append(city_info)
 
-        print(all_cities)
-
-    context = {'all_info': all_cities, 'form': form}
+    context = {'all_info': all_cities,
+               'form': form,
+               'message': message,
+               'message_class': message_class
+               }
 
     return render(request, 'weather/weather.html', context)
