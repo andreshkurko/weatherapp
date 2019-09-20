af5a74d949dc943f7a4b9e23879d523a27e131d2
CodingDevHub <codingdevhub@gmail.com>
2019-09-20 14:58:16 +0300

    added postgresql database

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -66,9 +66,14 @@ WSGI_APPLICATION = 'weatherapp.wsgi.application'
 
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'weatherapp_db',
+        'USER': 'weatherappuser',
+        'PASSWORD': 'A6559955A',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+
+   }
 }
 
 
